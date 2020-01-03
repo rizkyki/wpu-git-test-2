94b2cd9f5eb9157cf5db877940dca65679c4c825
rizkyki <rizkyinix@gmail.com>
2020-01-03 09:42:10 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -9,5 +9,10 @@
 <body>
     <h1>Hello World!</h1>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium vitae est, voluptas ducimus aut qui porro sapiente suscipit in, perferendis, ratione eveniet dignissimos quia consequatur enim soluta praesentium voluptatum tenetur.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+        <li>tiga</li>
+    </ul>
 </body>
 </html>
